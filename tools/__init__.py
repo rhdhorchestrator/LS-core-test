609e5f9f7f611bd9a5ae47189b5064b45244f791
Eloy Coto <eloy.coto@acalustra.com>
2025-08-25 13:40:53 +0200

    feat: add jsonschema rules
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,11 @@
 from .orchestrator_creation_workflow_rules import creation_workflow_rules
 from .orchestrator_get_sample_workflow import orchestrator_get_sample_workflow
+from .orchestrator_get_schema_rules import get_schema_rules
 from .orchestrator_workflow_renderer import orchestrator_preview_workflow
 
 __all__ = [
     creation_workflow_rules,
+    get_schema_rules,
     orchestrator_get_sample_workflow,
     orchestrator_preview_workflow,
 ]
